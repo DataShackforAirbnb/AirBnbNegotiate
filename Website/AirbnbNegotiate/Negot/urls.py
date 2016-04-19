dd8e7b5a9d389c149b2f64d5ffb430a6c832443a
Elena-Zhao <zhaoqing513@126.com>
2016-04-19 15:32:30 -0400

    user login finished

--- Website/AirbnbNegotiate/Negot/urls.py
+++ Website/AirbnbNegotiate/Negot/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     url(r'^about/', views.about, name = 'about'),
     url(r'^results/', views.results, name = 'results'),
     url(r'^search/', views.search, name = 'search'),
+    url(r'^auth_viwe', views.auth_view, name = 'auth_view'),
+    url(r'^logout', views.log_out, name = 'logout'),
 ]
--- Website/AirbnbNegotiate/Negot/views.py
+++ Website/AirbnbNegotiate/Negot/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .models import Search, User, Availability, Listing
 from django.core.urlresolvers import reverse
 import datetime
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+
 
 discount_threshold = 0.8
 def index(request):
@@ -37,3 +41,27 @@ def search(request):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return render(request, 'Negot/results.html', {'listings': results, 'checkin_date': checkin_date, 'checkout_date': checkout_date})
+
+def auth_view(request):
+    email = request.POST.get('username', 'qing')
+    password = request.POST.get('password', 'tb314630')
+    user = authenticate(username = email, password = password)
+
+    print 'email:', email, 'password:', password
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            # Redirect to a success page.
+            return render(request, 'Negot/index.html', {'user': user})
+        else:
+            return render(request, 'Negot/index.html', {'errors': 'Your account is inactive, please activate it before logging in.'})
+            # Return a 'disabled account' error message
+
+    else:
+        # Return an 'invalid login' error message.
+        return render(request, 'Negot/index.html', {'errors': 'Your username and password didn\'t match. Please try again.'})
+
+@login_required
+def log_out(request):
+    logout(request)
+    return render(request, 'Negot/index.html', {})
